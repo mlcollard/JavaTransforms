9ccb4df24fc7163a2842a735579150658c590f3a
Michael L. Collard <collard@uakron.edu>
2021-11-17 09:42:12 -0500

    Add functionality to Listener

--- JavaTransform.hpp
+++ JavaTransform.hpp
@@ -8,9 +8,48 @@
 #define INCLUDED_JAVATRANSFORM_HPP
 
 #include "JavaListenerBlock.h"
+#include <unordered_map>
+#include <string>
 
 class  JavaTransform : public JavaListener {
+private:
+    std::unordered_map<int, std::string> map{
+        { JavaParser::RuleWhile_stmt, "while"},
+        { JavaParser::RuleCondition, "condition"},
+    };
 
+public:
+
+    virtual void enterWhile_stmt(JavaParser::While_stmtContext *ctx) {
+    }
+
+    virtual void exitWhile_stmt(JavaParser::While_stmtContext *ctx) {
+    }
+
+
+    virtual void visitTerminal(antlr4::tree::TerminalNode *node) {
+
+        std::cout << node->getSymbol()->getText();
+    }
+
+    virtual void visitErrorNode(antlr4::tree::ErrorNode *node) {}
+
+    virtual void enterEveryRule(antlr4::ParserRuleContext *ctx) {
+
+        auto it = map.find(ctx->getRuleIndex());
+        if (it != map.end()) {
+            std::cout << "<" << it->second << ">";
+        }
+    }
+
+    virtual void exitEveryRule(antlr4::ParserRuleContext *ctx) {
+
+        auto it = map.find(ctx->getRuleIndex());
+        if (it != map.end()) {
+            std::cout << "</" << it->second << ">";
+        }
+
+    }
 };
 
 #endif
